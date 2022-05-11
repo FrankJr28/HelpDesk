bea3a42a46da86c40b8a3222e9b0ceb67397d739
FrankJr28 <francisco.vasquezjr@alumnos.udg.mx>
2022-05-10 20:37:47 -0500

    Se agrega base de datos bien

--- componentes/principal/insertarTicket.php
+++ componentes/principal/insertarTicket.php
@@ -105,13 +105,13 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'java10028@gmail.com';                     //SMTP username
-        $mail->Password   = 'PagaN1728';                               //SMTP password
+        $mail->Username   = '';                     //SMTP username
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('java10028@gmail.com', 'Javier Vasquez');
+        $mail->setFrom('gmail.com', '');
         $mail->addAddress($corI);     //Add a recipient
 
         //Content
